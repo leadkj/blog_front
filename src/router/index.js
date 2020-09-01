8ccd28223c20ca37d1768831150ff63811aa312f
leadkj <wjx010107@163.com>
2020-09-01 13:37:53 +0800

    'updatetitle'

--- src/router/index.js
+++ src/router/index.js
@@ -10,49 +10,72 @@ import ArticleTag from "../components/ArticleTag";
 Vue.use(VueRouter)
 
 const routes = [
-	{
-		path: '/',
-		redirect: '/index'
-	},
-	{
-		path: '/index',
-		name: 'Index',
-		component: IndexCpn
-	},
-	{
-		path: '/categorys/:id',
-		name: 'List_Category',
-		component: ContentCpn
-	},
-	{
-		path: '/tags/:id',
-		name: 'List_Tag',
-		component: ArticleTag
-	},
-	{
-		path: '/articles/:id',
-		name: 'Detail',
-		component: Article
-	},
-	{
-		path: '/search/:keyword',
-		name: 'search',
-		component: SearchCpn
-	}
+    {
+        path: '/',
+        redirect: '/index',
+        meta: {
+            title: '我的博客'
+        }
+    },
+    {
+        path: '/index',
+        name: 'Index',
+        component: IndexCpn,
+        meta: {
+            title: '我的博客',
+        }
+    },
+    {
+        path: '/categorys/:id',
+        name: 'List_Category',
+        component: ContentCpn,
+        meta:{
+            title: '文章列表'
+        }
+    },
+    {
+        path: '/tags/:id',
+        name: 'List_Tag',
+        component: ArticleTag,
+        meta: {
+            title: '文章列表'
+        }
+    },
+    {
+        path: '/articles/:id',
+        name: 'Detail',
+        component: Article,
+        meta: {
+            title: '文章详情'
+        }
+    },
+    {
+        path: '/search/:keyword',
+        name: 'search',
+        component: SearchCpn,
+        meta: {
+            title: '搜索结果'
+        }
+    }
 
 ]
 
 
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
-	return originalPush.call(this, location).catch(err => err)
+    return originalPush.call(this, location).catch(err => err)
 }
 
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
+router.beforeEach((to, from, next) => {
 
+    document.title = to.meta.title
+    console.log(to);
+    next()
+})
 export default router
